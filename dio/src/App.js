f3ea1cf9def68c0af6ecdf27576b0a0239764e25
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2023-01-21 18:20:00 -0300

    Criando um componente de botão

---
+++ dio/src/App.js
@@ -0,0 +1,16 @@
+import Button from "./components/Button";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React</h1>
+      <button title="Entrar"/>
+      <button title="Fechar"/>
+      <button title="Abrir"/>
+      <button title="Sair"/>
+    </div>
+  );
+}
+
+export default App;
