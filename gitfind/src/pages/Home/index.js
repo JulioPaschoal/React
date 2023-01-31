dee3873f7e51dbac92aac16020ef2c0146e091e3
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2023-01-31 13:24:46 -0300

    Criando navegação entre paginas

--- gitfind/src/pages/Home/index.js
+++ gitfind/src/pages/Home/index.js
@@ -9,12 +9,18 @@ function App() {
       <div className="container">
         <img src={background} className="background" alt="background app" />
         <div className="info">
-          <input name="usuario" placeholder="@username"/>
+          <input name="usuario" placeholder="@username" />
           <button>Buscas</button>
         </div>
       </div>
+      <div className="perfil">
+          <img src="https://avatars.githubusercontent.com/u/17685764?s=96&v=4" className="profile" alt="Imagem Perfil" />
+          <div>
+            <h3>JULIO PASCHOAL</h3>
+            <p> Descrição</p>
+          </div>
+        </div>
     </div>
   );
 }
-
 export default App;
