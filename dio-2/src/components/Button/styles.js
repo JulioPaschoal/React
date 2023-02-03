83308e3ceca5597decd47250ab0da5444a9a24d6
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2023-02-03 12:57:13 -0300

    Atualização

--- dio-2/src/components/Button/styles.js
+++ dio-2/src/components/Button/styles.js
@@ -1,19 +1,21 @@
-import styled, { css } from 'styled-components';
+import styled, {css}  from 'styled-components';
 
 export const ButtonContainer = styled.button`
     background: #565656;
     border-radius: 22px;
     position: relative;
+
     color: #FFFFFF;
     padding: 2px 12px;
     min-width: 120px;
     width: 100%;
     
-    ${({ variant }) => variant !== "primary" && css`
+    ${({variant}) => variant !== "primary" && css`
         min-width: 167px;
         height: 33px;
         
         background: #E4105D;
+
         &::after {
             content: '';
             position: absolute;
---
+++ dio-2/src/pages/feed/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Card } from '../../components/Card';
+import { UserInfo } from '../../components/UserInfo';
+import { Header } from '../../components/Header';
+
+import { Container, Column, Title, TitleHighlight } from './styles';
+
+const Feed = () => {
+  return (
+    <>
+        <Header autenticado={true}/>
+        <Container>
+            <Column flex={3}>
+                <Title>Feed</Title>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </Column>
+            <Column flex={1}>
+              <TitleHighlight> # RANKING 5 TOP DA SEMANA </TitleHighlight>
+                <UserInfo nome="Julio Paschoal" image="https://avatars.githubusercontent.com/u/17685764?s=" percentual={25}/>
+                <UserInfo nome="Julio Paschoal" image="https://avatars.githubusercontent.com/u/17685764?s=" percentual={65}/>
+                <UserInfo nome="Julio Paschoal" image="https://avatars.githubusercontent.com/u/17685764?s=" percentual={45}/>
+                <UserInfo nome="Julio Paschoal" image="https://avatars.githubusercontent.com/u/17685764?s=" percentual={72}/>
+            </Column>
+        </Container>
+    </>
+  )
+}
+
+export { Feed }; 
